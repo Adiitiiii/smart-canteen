d8b829131aa0d0e3159b625973aabafc066071d2
Sahil Pabale <dev.sahilpabale@gmail.com>
2023-02-28 22:54:48 +0530

    changed firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: 'G-34156ZR8GM',
+  measurementId: 'G-M8WY580DMX',
 };
 
 // Initialize Firebase
