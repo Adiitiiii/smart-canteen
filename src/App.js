770f8067104c5bd6f66ea36d0017cb3a3c5f4978
Sahil Pabale <dev.sahilpabale@gmail.com>
2023-04-02 10:36:45 +0530

    uncommented the routes

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
-          {/* <Route path="/menu" element={<Auth />} /> */}
-          {/* <Route path="/menu/:id" element={<Auth />} /> -> useParam() Hook to get the param*/}
-          {/* <Route
+          <Route path="/menu" element={<Auth />} />
+          <Route path="/menu/:id" element={<Auth />} />
+          <Route
             path="/menu"
             element={
               <Protected>
@@ -43,7 +43,7 @@ function App() {
                 <Admin />{' '}
               </Protected>
             }
-          /> */}
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </AuthContextProvider>
